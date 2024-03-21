3c730268cda104b2f1b28e3ec3f092bfaad54795
AkhilaCheryala <akhilacheryala2804@gmail.com>
2024-03-21 11:22:40 +0530

    form positioning

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -101,7 +101,7 @@ return(
        </div>
     </div>
     <h2 className='reviews'>Course Reviews</h2>
-    <div className="feedbacks  d-flex justify-content-center ">
+    <div className="feedbacks  d-flex justify-content-center  position-static">
      <div className='row'>
        <div className='col-1'>
        <div className='card  p-2 '>
@@ -135,21 +135,7 @@ return(
       <h5 className='quote'>"In the ink of truth, journalism writes history's authentic tale."</h5>
       
     <Footer/>
-     {/* Bootstrap CSS */}
-     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossOrigin="anonymous" />
      
-     {/* Bootstrap Icons */}
-     <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.8.1/font/bootstrap-icons.css" rel="stylesheet" />
-     
-     {/* jQuery (necessary for Bootstrap's JavaScript plugins) */}
-     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-     
-     {/* Popper.js */}
-     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
-     
-     {/* Bootstrap JS */}
-     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js" integrity="sha384-KyZXEAg3QhqLMpG8r+Knujsl5+gbg2Jj13KBOePhv7ta9tk+1O6wlsVdeux0lf1r" crossOrigin="anonymous"></script>
-
    
 {showSignup && <div className="signup-overlay"><Signup onClose={toggleSignup}/></div>}
  </div> 
